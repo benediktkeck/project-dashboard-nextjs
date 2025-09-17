56402d527b0b93ca0bb444917ab056e37d883aca
Benedikt Keck <benedikt.keck@gmail.com>
2025-09-17 21:58:34 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# project-dashboard-nextjs
+# keckdev.de
+
+this is my personal portfolio website representing me. work is still in progress
+
+## Tech Stack
+
+- Next.js **15** (App Router)
+- React 19
+- TypeScript
+- Tailwind CSS
+- Lucide Icons
---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Karla } from "next/font/google";
+import "./globals.css";
+
+const karla = Karla({
+    variable: "--font-karla",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Project Dashboard",
+    icons: [
+        { rel: "icon", url: "/icon.png" },
+        { rel: "apple-touch-icon", url: "/apple-icon.png" },
+    ]
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="de" suppressHydrationWarning>
+        <body
+            className={`${karla.className} antialiased min-h-screen bg-light dark:bg-background text-dark dark:text-white`}
+        >
+        <div>{children}</div>
+        </body>
+        </html>
+    );
+}
